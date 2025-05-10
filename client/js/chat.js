93b68574bea6af607f78374853177af24a268ecc
Aven <semrau.j@web.de>
2025-05-11 01:01:32 +0200

    fix chat and user list scrollbar

--- client/js/chat.js
+++ client/js/chat.js
@@ -40,7 +40,6 @@ function handleChatMessages(chatMessages) {
   chatContainer.innerHTML = '';
 
   function hashColor(str) {
-    
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
       hash = str.charCodeAt(i) + ((hash << 5) - hash);
