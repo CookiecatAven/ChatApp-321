3265c7ad1018fdf3c9fdc5c832134a1e25b578f5
Aven <semrau.j@web.de>
2025-05-10 22:31:53 +0200

    add user form for changing the name

--- client/js/profile.js
+++ client/js/profile.js
@@ -12,3 +12,25 @@ function updateUIForAuthenticatedUser() {
   document.getElementById('user-avatar').src = user?.picture ?? '';
   document.getElementById('user-name').textContent = user?.name ?? '';
 }
+
+function displayEditUserNameForm(showForm = true) {
+  document.getElementById('user-name-input').value = user?.name ?? '';
+  document.getElementById('user-info').classList.toggle('hidden', showForm);
+  document.getElementById('user-info-form').classList.toggle('hidden', !showForm);
+}
+
+function handleUserNameFormSubmit(e) {
+  e.preventDefault();
+  const newName = document.getElementById('user-name-input').value;
+  socket.send(JSON.stringify({
+    type: 'update-name',
+    data: {name: newName}
+  }));
+  e.target.remove();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('edit-name-button').addEventListener('click', displayEditUserNameForm);
+  document.getElementById('cancel-edit-name-button').addEventListener('click', () => displayEditUserNameForm(false));
+  document.getElementById('user-info-form').addEventListener('submit', handleUserNameFormSubmit);
+});
