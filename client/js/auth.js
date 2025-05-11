ec8587046884af8bc06ae8436d6ca15557629c63
Aven <semrau.j@web.de>
2025-05-11 16:33:32 +0200

    fix issue where Google init would be tried indefinitely

--- client/js/auth.js
+++ client/js/auth.js
@@ -42,6 +42,8 @@ function handleCredentialResponse(response) {
   }
 }
 
+let initCounter = 0;
+
 /**
  * Initializes Google Sign-In by setting up the Google client library with the required
  * client ID and callback function. Additionally, it renders the Google Sign-In button
@@ -49,9 +51,14 @@ function handleCredentialResponse(response) {
  */
 function initializeGoogleSignIn() {
   if (!window.google?.accounts?.id) {
-    console.warn('Google client library isn\'t loaded! Deferring initialization of Google Sign-In.');
-    setTimeout(initializeGoogleSignIn, 1000);
-    return;
+    if (initCounter <= 5) {
+      console.warn('Google client library isn\'t loaded! Deferring init of Google Sign-In.');
+      setTimeout(initializeGoogleSignIn, 1000);
+      initCounter++;
+      return;
+    } else {
+      console.error(`Google client library didn't load during ${initCounter} seconds. Aborting init of Google Sign-In.`);
+    }
   }
   loadConfig().then(() => {
     window.google.accounts.id.initialize({
